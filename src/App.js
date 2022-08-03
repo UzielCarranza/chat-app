24b7a8c88b6a1817058dfe8c6686746c5b96f405
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-03 17:39:09 -0400

    adding routes to display different pages within the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './styling/App.css';
-import {Login} from './components/Login'
+import {Login} from './components/Login';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-                <Login/>
+            <Routes>
+                <Route path="/" element={<Login/> }/>
+            </Routes>
         </div>
+
     );
 }
 
