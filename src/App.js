60b1f95e3409371b58362d10b3847e962917acfc
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-05 21:45:00 -0400

    update routes to render PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './styling/App.css';
 import {Login} from './components/Login';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Switch} from "react-router-dom";
 import {HomePage} from "./components/HomePage";
 import {SignUp} from "./components/SignUp";
+import {PrivateRoute} from "./auth/PrivateRoute";
 
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/login" element={<Login/> }/>
+                <Route path="/" element={<PrivateRoute/>}/>
+                <Route path="/login" element={<Login/>}/>
 
-                <Route path="/home" element={<HomePage/> }/>
-                <Route path="/signup" element={<SignUp/> }/>
+                <Route path="/signup" element={<SignUp/>}/>
+
             </Routes>
+
         </div>
 
-    );
+    )
+        ;
 }
 
 export default App;
