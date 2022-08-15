4c6f552f625bd6b24a2ba3f6ab1f7f9a9ad8dda4
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-15 13:57:39 -0400

    separation of concerns... took data validation code block into a separate funciton

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -87,6 +87,48 @@ export const SignUp = () => {
         }, [password]
     )
 
+    const dataValidation = () => {
+        return showValidation &&
+            <div id="data-validation" className="flex flex-col">
+                {isPasswordGreaterThan8 ?
+                    <div className="flex justify-start"><AiFillCheckCircle style={{color: 'green'}}/><small>password
+                        must be 8 characters or more</small></div> :
+                    <div className="flex justify-start"><MdDoNotTouch style={{color: 'red'}}/><small>password
+                        must
+                        be 8
+                        characters or more</small></div>}
+                {isUpperCase ?
+                    <div className="flex justify-start mt-2"><AiFillCheckCircle
+                        style={{color: 'green'}}/><small>Must
+                        contain an upper case letter</small></div> :
+                    <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>Must
+                        contain
+                        an upper case letter</small></div>}
+                {isLowerCase ?
+                    <div className="flex justify-start mt-2"><AiFillCheckCircle
+                        style={{color: 'green'}}/><small>Must
+                        contain a lower case letter</small></div> :
+                    <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>Must
+                        contain
+                        a lower case letter</small></div>}
+                {isNumericValue ?
+                    <div className="flex justify-start mt-2"><AiFillCheckCircle
+                        style={{color: 'green'}}/><small>Must
+                        contain a numeric value</small></div> :
+                    <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>
+                        Must
+                        contain a numeric value</small></div>}
+                {hasSpecialCharacters ?
+                    <div className="flex justify-start mt-2"><AiFillCheckCircle
+                        style={{color: 'green'}}/><small>Must
+                        contain at least one special character</small></div> :
+                    <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>
+                        Must
+                        contain at least one special character</small></div>}
+            </div>
+
+    }
+
 //let's you navigate to other pages programmatically
     const navigate = useNavigate()
 
@@ -133,45 +175,8 @@ export const SignUp = () => {
                         required
                     />
                 </div>
-                {showValidation &&
-                    <div id="data-validation" className="flex flex-col">
-                        {isPasswordGreaterThan8 ?
-                            <div className="flex justify-start"><AiFillCheckCircle style={{color: 'green'}}/><small>password
-                                must be 8 characters or more</small></div> :
-                            <div className="flex justify-start"><MdDoNotTouch style={{color: 'red'}}/><small>password
-                                must
-                                be 8
-                                characters or more</small></div>}
-                        {isUpperCase ?
-                            <div className="flex justify-start mt-2"><AiFillCheckCircle
-                                style={{color: 'green'}}/><small>Must
-                                contain an upper case letter</small></div> :
-                            <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>Must
-                                contain
-                                an upper case letter</small></div>}
-                        {isLowerCase ?
-                            <div className="flex justify-start mt-2"><AiFillCheckCircle
-                                style={{color: 'green'}}/><small>Must
-                                contain a lower case letter</small></div> :
-                            <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>Must
-                                contain
-                                a lower case letter</small></div>}
-                        {isNumericValue ?
-                            <div className="flex justify-start mt-2"><AiFillCheckCircle
-                                style={{color: 'green'}}/><small>Must
-                                contain a numeric value</small></div> :
-                            <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>
-                                Must
-                                contain a numeric value</small></div>}
-                        {hasSpecialCharacters ?
-                            <div className="flex justify-start mt-2"><AiFillCheckCircle
-                                style={{color: 'green'}}/><small>Must
-                                contain at least one special character</small></div> :
-                            <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>
-                                Must
-                                contain at least one special character</small></div>}
-                    </div>
-                }
+                {dataValidation()}
+
                 {/*confirm password field*/}
                 <div className="divider">
                     <input
