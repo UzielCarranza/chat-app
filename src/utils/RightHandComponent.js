523d5d6497c07a87569e24653f3cfe7143b36fd3
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-17 21:40:23 -0400

    created component to act as right view of the home screen... will be displaying the current chat of an user

--- src/utils/RightHandComponent.js
+++ src/utils/RightHandComponent.js
@@ -1,6 +1,6 @@
 
-export const LeftHandComponent = () => {
-    return <ol className="mt-0 h-full" style={{backgroundColor: 'green'}}>
+export const RightHandComponent = () => {
+    return <ol className="mt-0 h-full" style={{backgroundColor: 'grey'}}>
         <li>Here goes user information and start a new message</li>
         <li>a search bar to look for a contact</li>
         <li>all messages history</li>
