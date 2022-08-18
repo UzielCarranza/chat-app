9b5de12b697bd50627ac3920256db0b037cd4a8d
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-17 21:42:46 -0400

    created component to display left view of the home screen... will be displaying history chat with other users

--- src/utils/LeftHandComponent.js
+++ src/utils/LeftHandComponent.js
@@ -1,4 +1,5 @@
 export const LeftHandComponent = () => {
+    //will display the left side of the screen
     return <ol className="mt-0 h-full" style={{backgroundColor: 'green'}}>
         <li>Here goes user information and start a new message</li>
         <li>a search bar to look for a contact</li>
