e9fcdf5bc4da7f9daa3472dbece5b43eda6b6c9d
UzielCarranza <uzielcarranzac97@gmail.com>
2022-08-23 10:39:38 -0400

    removed all props that were passed dowm to the component to received them as an object instead for better readability and less obstruction

--- src/components/signup/showDataOnPasswordValidation.js
+++ src/components/signup/showDataOnPasswordValidation.js
@@ -1,38 +1,38 @@
 import {AiFillCheckCircle} from "react-icons/ai";
 import {MdDoNotTouch} from "react-icons/md";
 
-export const dataValidation = (showValidation, isPasswordGreaterThan8, isUpperCase, isLowerCase, isNumericValue, hasSpecialCharacters) => {
+export const showDataOnPasswordValidation = (showValidation, passWordRequirements) => {
     return showValidation &&
         <div id="data-validation" className="flex flex-col">
-            {isPasswordGreaterThan8 ?
+            {passWordRequirements.isPasswordGreaterThan8 ?
                 <div className="flex justify-start"><AiFillCheckCircle style={{color: 'green'}}/><small>password
                     must be 8 characters or more</small></div> :
                 <div className="flex justify-start"><MdDoNotTouch style={{color: 'red'}}/><small>password
                     must
                     be 8
                     characters or more</small></div>}
-            {isUpperCase ?
+            {passWordRequirements.isUpperCase ?
                 <div className="flex justify-start mt-2"><AiFillCheckCircle
                     style={{color: 'green'}}/><small>Must
                     contain an upper case letter</small></div> :
                 <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>Must
                     contain
                     an upper case letter</small></div>}
-            {isLowerCase ?
+            {passWordRequirements.isLowerCase ?
                 <div className="flex justify-start mt-2"><AiFillCheckCircle
                     style={{color: 'green'}}/><small>Must
                     contain a lower case letter</small></div> :
                 <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>Must
                     contain
                     a lower case letter</small></div>}
-            {isNumericValue ?
+            {passWordRequirements.isNumericValue ?
                 <div className="flex justify-start mt-2"><AiFillCheckCircle
                     style={{color: 'green'}}/><small>Must
                     contain a numeric value</small></div> :
                 <div className="flex justify-start mt-2"><MdDoNotTouch style={{color: 'red'}}/><small>
                     Must
                     contain a numeric value</small></div>}
-            {hasSpecialCharacters ?
+            {passWordRequirements.hasSpecialCharacters ?
                 <div className="flex justify-start mt-2"><AiFillCheckCircle
                     style={{color: 'green'}}/><small>Must
                     contain at least one special character</small></div> :
